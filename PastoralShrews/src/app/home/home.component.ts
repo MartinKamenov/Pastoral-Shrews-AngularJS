262f3ca2611d636e8eab43fa1c63440dbce96621
Martin Kamenov <martykam@abv.bg>
2017-08-31 14:49:54 +0300

    Added template for homepage.

--- PastoralShrews/src/app/home/home.component.ts
+++ PastoralShrews/src/app/home/home.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['../../assets/vendor/bootstrap/css/bootstrap.css',
+  '../../assets/css/shop-homepage.css']
 })
 export class HomeComponent implements OnInit {
 
