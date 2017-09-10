7836bfafb3533e6680123e37260f5ed63e40fd40
Martin Kamenov <martykam@abv.bg>
2017-09-10 13:13:00 +0300

    Changed the euro pipe so it shows correct value.

--- PastoralShrews/src/app/share/pipes/change-valute.pipe.ts
+++ PastoralShrews/src/app/share/pipes/change-valute.pipe.ts
@@ -5,10 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatFileSizePipe implements PipeTransform {
   transform(valueInDollars: string): string {
-    const euroToDollar = 1.203535;
+    const euroToDollar = 0.83088;
     const valueInDollarsNum = +valueInDollars;
     const valueInEuro = valueInDollarsNum * euroToDollar;
-    const result = Math.round(valueInEuro);
-    return `${result}`;
+    const result = (String)(Math.round(valueInEuro));
+    let result2 = '';
+    const resultCount = result.length;
+    for (let i = 0; i < resultCount; i++) {
+      if ((i % 3) === 0 && i !== 0) {
+        result2 = ',' + result2;
+      }
+
+      result2 = result[resultCount - i - 1] + result2;
+    }
+    return `${result2}`;
   }
 }
